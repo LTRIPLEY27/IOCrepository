d5a845ded57a2e271f6541a5ce4a8a703847b8e6
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-10-16 14:11:22 +0200

    reordenamiento

--- IOCProjects_DAW/src/m03/Ejercicios/Agenda.java
+++ IOCProjects_DAW/src/m03/Ejercicios/Agenda.java
@@ -15,6 +15,23 @@ public class Agenda {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int [] array = {3,4,10,8,7};
+        int aux = 0;
+        for(int i = 0; i < array.length; i++) {
+            for(int j = (i + 1); j < array.length; j++) { // el valor de j al estar contenido en i se ira incrementando y con ello la posicion
+                if(array[i] > array[j]) { // si la posicion original es mayor a la siguiente
+                aux = array[i]; // guarda posicion original
+                array[i] = array[j]; // posicion original adquiere el valor de posicion siguiente
+                array[j] = aux; // posicion siguiente adquiere el valor mayor, AL IR ITERANDO, LOS VALORES MAYORES SE CORRERAN A ULTIMAS INSTANCIAS Y DE ESA MANERA SE REORDENARA
+                }
+            }
+            
+        }
+        
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
     
 }
